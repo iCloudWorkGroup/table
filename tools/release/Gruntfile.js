5adf6a6794b5ef5c48e1b6fc599c3736fc0a2565
wlchair <wulei0114@126.com>
2017-06-13 17:12:56 +0800

    fix(tool): commit功能，跟fengniao项目发布效果统一 (#34)

--- tools/release/Gruntfile.js
+++ tools/release/Gruntfile.js
@@ -5,7 +5,6 @@ module.exports = function(grunt) {
         bump: {
             options: {
                 updateConfigs: ['pkg'],
-                commit: false,
                 commitFIles: ['package.json', '../../CHANGELOG.md'],
                 commitMessage: 'spreadsheet: release v%VERSION%',
                 push: false
